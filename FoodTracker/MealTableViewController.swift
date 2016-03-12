309266c55816991338eaf7ecd576f3df8311d6fe
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 18:47:39 -0500

    adds reference to the data model in mealtableviewcontroller

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MealTableViewController: UITableViewController {
 
+    //MARK: Properties
+    var meals = [Meal]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
