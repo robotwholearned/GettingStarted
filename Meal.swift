7b18022f43d1c4e548d49c31e31821f23717fc65
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 11:04:03 -0500

    adds meal class to app and test target

---
+++ Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Sandquist, Cassandra - Cassandra on 3/12/16.
+//  Copyright © 2016 robotwholearned. All rights reserved.
+//
+
+import Foundation
