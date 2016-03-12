60a8b1d3e443f261a4644fce86b9beb68378ea8c
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 07:50:15 -0500

    adds a connection (action) from button to view controller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
 
-    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+    }
+
 }
 
