9e4560362915f012c70df6e8fa72b683891fda7a
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 09:18:33 -0500

    adds a red button to ratingcontrol

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -14,6 +14,11 @@ class RatingControl: UIView {
     //Every UIView subclass that implements an initializer must include an implementation of init?(coder:)
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = .redColor()
+
+        addSubview(button)
     }
 
 }
