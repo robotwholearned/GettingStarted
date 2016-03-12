80daf7155f047c85c7149f58796377542bb0dec6
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 09:28:55 -0500

    iterates 5 times and adds a button (with the same frame) to the view

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -19,12 +19,22 @@ class RatingControl: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
 
+        for _ in 0..<5 {
+            addButton()
+        }
+    }
+
+    func addButton() {
+
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
 
         button.backgroundColor = .redColor()
         button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
 
+        ratingButtons += [button]
+
         addSubview(button)
+
     }
 
     override func intrinsicContentSize() -> CGSize {
