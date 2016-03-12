68819f3f2fe4ca4c7ac98bc7cd761395044e2103
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 18:56:25 -0500

    adds sample meals

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+enum RatingLevels: Int {
+    case One = 1, Two, Three, Four, Five
+}
+
+struct MealNames {
+    static let capreseSalad = "Caprese Salad"
+    static let chickenAndPotatoes = "Chicken and Potatoes"
+    static let pastaAndMeatballs = "Pasta with Meatballs"
+}
+
 class MealTableViewController: UITableViewController {
 
     //MARK: Properties
     var meals = [Meal]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +31,19 @@ class MealTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    func loadSampleMeals() {
+        let photo1 = R.image.meal1()
+        let meal1 = Meal(name: MealNames.capreseSalad, photo: photo1, rating: RatingLevels.Four.rawValue)!
+
+        let photo2 = R.image.meal2()
+        let meal2 = Meal(name: MealNames.chickenAndPotatoes, photo: photo2, rating: RatingLevels.Five.rawValue)!
+
+        let photo3 = R.image.meal3()
+        let meal3 = Meal(name: MealNames.pastaAndMeatballs, photo: photo3, rating: RatingLevels.Three.rawValue)!
+
+        meals += [meal1, meal2, meal3]
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,7 +89,7 @@ class MealTableViewController: UITableViewController {
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
