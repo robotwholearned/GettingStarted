d02b1e5f5e8ea534a1f8dfcd7c22e7d9e1237033
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 09:12:41 -0500

    adds RatingControl view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Sandquist, Cassandra - Cassandra on 3/12/16.
+//  Copyright © 2016 robotwholearned. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
