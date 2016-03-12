0297db3b8d842c6cd5686d6cbbe00aabfeab1a1e
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 09:24:31 -0500

    attachs the action ratingButtonTapped: to the button object, which will be triggered whenever the .TouchDown event occurs

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -16,7 +16,9 @@ class RatingControl: UIView {
         super.init(coder: aDecoder)
 
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+
         button.backgroundColor = .redColor()
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
 
         addSubview(button)
     }
