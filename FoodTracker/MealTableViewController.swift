1de7c6cf37d0748e5bdd1ce6f6e315f3609abfd1
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-12 22:04:17 -0500

    checks if sender is mealViewController, and that there is a valid meal, then adds meal to datasource, and inserts a new row at the bottom of the meals tableview controller

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -74,6 +74,14 @@ class MealTableViewController: UITableViewController {
         return cell
     }
 
+    @IBAction func unwindToMealList (segue: UIStoryboardSegue) {
+        if let sourceViewController = segue.sourceViewController as? MealViewController, meal = sourceViewController.meal {
+            // Add a new meal.
+            let newIndexPath = NSIndexPath(forRow: meals.count, inSection: 0)
+            meals.append(meal)
+            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
