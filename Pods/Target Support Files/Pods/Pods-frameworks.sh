bcf240522234ccc2224a596a349c678cdfe85b50
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-20 13:10:30 -0400

    adds pod Former

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/Former.framework"
   install_framework "Pods/Rswift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/Former.framework"
   install_framework "Pods/Rswift.framework"
 fi
