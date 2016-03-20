d06c1b7ba77605b2c7e3d2102c9f6c5c0e2d603d
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-20 13:10:01 -0400

    fixes pod target issue

--- Podfile
+++ Podfile
@@ -1,13 +1,6 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '8.0'
 # Uncomment this line if you're using Swift
- use_frameworks!
+use_frameworks!
 
-target 'FoodTracker' do
- pod 'R.swift'
-end
-
-target 'FoodTrackerTests' do
-
-end
-
+pod 'R.swift'
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-FoodTracker/Rswift.framework"
+  install_framework "Pods/Rswift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-FoodTracker/Rswift.framework"
+  install_framework "Pods/Rswift.framework"
 fi
